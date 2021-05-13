5e221a248801aba85677b0d278314a7ea7b34a41
Kamil <gaeckikamil@gmail.com>
2021-05-13 19:26:39 +0200

    Update GeoPanda-Polska.py

--- GeoPanda-Polska.py
+++ GeoPanda-Polska.py
@@ -168,3 +168,119 @@ plt.autoscale(True)
 ax.set_axis_on()
 plt.axis('equal')
 plt.title('Liczba ludności w województwach wiek 15-65')
+
+#%%
+import geopandas as gpd
+import pandas as pd
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+
+
+gdf=gpd.read_file('PD_STAT_GRID_CELL_2011.shp')
+gdf=gdf.to_crs("EPSG:4326")
+#gdf.plot("TOT",legend=True)
+gdf['centroid']=gdf.centroid
+gdw=gpd.read_file('Województwa.shp')
+gdw=gdw.to_crs("EPSG:4326")
+gdw.plot(legend=True)
+cell = gpd.GeoDataFrame(gdw, columns=['geometry'])
+ax=gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_65__', cmap='jet')
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='blue')
+ax.axis("on")
+merged = gpd.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_65__'] = dissolve.TOT_65__.values
+ax = cell.plot(column='TOT_65__', figsize=(12, 8), cmap='viridis', edgecolor="blue", legend = True)
+plt.autoscale(True)
+ax.set_axis_on()
+plt.axis('equal');
+plt.title('ludnosc 65 plus')
+#%%
+import geopandas as gpd
+import pandas as pd
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+gdf=gpd.read_file('PD_STAT_GRID_CELL_2011.shp')
+gdf=gdf.to_crs("EPSG:4326")
+gdf.plot("TOT",legend=True)
+gdf['centroid']=gdf.centroid
+gdw=gpd.read_file('Województwa.shp')
+gdw=gdw.to_crs("EPSG:4326")
+gdw.plot(legend=True)
+cell = gpd.GeoDataFrame(gdw, columns=['geometry'])
+ax=gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_MALE_0_14', cmap='jet')
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("on")
+merged = gpd.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_MALE_0_14'] = dissolve.TOT_MALE_0_14.values
+ax = cell.plot(column='TOT_MALE_0_14', figsize=(12, 8), cmap='viridis', edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_on()
+plt.axis('equal');
+plt.title('mężczyźni do 14 lat')
+#%%
+import geopandas as gpd
+import pandas as pd
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+gdf=gpd.read_file('PD_STAT_GRID_CELL_2011.shp')
+gdf=gdf.to_crs("EPSG:4326")
+gdf.plot("TOT",legend=True)
+gdf['centroid']=gdf.centroid
+gdw=gpd.read_file('Województwa.shp')
+gdw=gdw.to_crs("EPSG:4326")
+gdw.plot(legend=True)
+cell = gpd.GeoDataFrame(gdw, columns=['geometry'])
+ax=gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_MALE_15_64', cmap='jet')
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("on")
+merged = gpd.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_MALE_15_64'] = dissolve.TOT_MALE_15_64.values
+ax = cell.plot(column='TOT_MALE_15_64', figsize=(12, 8), cmap='viridis', edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_on()
+plt.axis('equal');
+plt.title('mężczyzn1 15-64')
+#%%
+import geopandas as gpd
+import pandas as pd
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+gdf=gpd.read_file('PD_STAT_GRID_CELL_2011.shp')
+gdf=gdf.to_crs("EPSG:4326")
+#gdf.plot("TOT",legend=True)
+gdf['centroid']=gdf.centroid
+gdw=gpd.read_file('Województwa.shp')
+gdw=gdw.to_crs("EPSG:4326")
+#gdw.plot(legend=True)
+cell = gpd.GeoDataFrame(gdw, columns=['geometry'])
+ax=gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_MALE_65__', cmap='jet')
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("on")
+merged = gpd.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_MALE_65__'] = dissolve.TOT_MALE_65__.values
+ax = cell.plot(column='TOT_MALE_65__', figsize=(12, 8), cmap='viridis', edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_on()
+plt.axis('equal');
+plt.title('męźczyźni 65 plus')
